3a19bde600843d105d376b651dc5ab0b622030cf
Filip <66736221+filipbartek6417@users.noreply.github.com>
2021-05-17 12:56:59 +0200

    stage 3

--- Memorization Tool/task/tool.py
+++ Memorization Tool/task/tool.py
@@ -15,8 +15,8 @@ class Flashcard(Base):  # Declaration of the db table
     answer = Column(String)
 
 
-session.query(Flashcard).delete()  # These 2 lines clear the db
-session.commit()
+# session.query(Flashcard).delete()  # These 2 lines clear the db
+# session.commit()
 Base.metadata.create_all(engine)  # Initializes the db
 
 while (inp1 := input(f'1. Add flashcards\n'
@@ -40,14 +40,31 @@ while (inp1 := input(f'1. Add flashcards\n'
         # iterate over all flashcards from db
         if flashcards := session.query(Flashcard).all():
             for card in flashcards:
-                print(f'Question: {card.question}')
-                while (ans := input(f'Please press "y" to see the answer '
-                                    f'or press "n" to skip:\n')) not in ['y', 'n']:
-                    pass
+                print(f'Question: {card.question}:')
+                while (ans := input(f'press "y" to see the answer:\n'
+                                    f'press "n" to skip:\n'
+                                    f'press "u" to update:\n')) not in ['y', 'n', 'u']:
+                    print(f'{ans} is not an option')
                 if ans == 'y':
                     print(f'Answer: {card.answer}')
+                elif ans == 'u':
+                    while (opt := input(f'press "d" to delete the flashcard:\n'
+                                       f'press "e" to edit the flashcard:\n')) not in ['d', 'e']:
+                        print(f'{opt} is not an option')
+                    if opt == 'd':
+                        # delete the flashcard
+                        session.delete(card)
+                    else:
+                        # edit the flashcard
+                        if (new_question := input(f'current question: {card.question}\n'
+                                                  f'please write a new question:\n')) != '':
+                            card.question = new_question
+                        if (new_answer := input(f'current answer: {card.answer}\n'
+                                                f'please write a new answer:\n')) != '':
+                            card.answer = new_answer
+                    session.commit()
         else:
-            print('There is no flashcard to practice!')
+            print('There are no flashcards to practice!')
     else:
         print(f'{inp1} is not an option')
 print('Bye!')
