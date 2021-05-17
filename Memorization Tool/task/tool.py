580ab85e194188bdd7d4232c04c141b1354226e2
Filip <66736221+filipbartek6417@users.noreply.github.com>
2021-05-17 11:06:40 +0200

    stage 2

--- Memorization Tool/task/tool.py
+++ Memorization Tool/task/tool.py
@@ -1,4 +1,24 @@
-flashcards = {}
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///flashcard.db?check_same_thread=False')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class Flashcard(Base):  # Declaration of the db table
+    __tablename__ = 'flashcard'
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+
+
+session.query(Flashcard).delete()  # These 2 lines clear the db
+session.commit()
+Base.metadata.create_all(engine)  # Initializes the db
+
 while (inp1 := input(f'1. Add flashcards\n'
                      f'2. Practice flashcards\n'
                      f'3. Exit\n')) != '3':
@@ -7,24 +27,25 @@ while (inp1 := input(f'1. Add flashcards\n'
         while (inp2 := input(f'1. Add a new flashcard\n'
                              f'2. Exit\n')) != '2':
             if inp2 == '1':
-                # add a flash card
+                # add a flash card to db
                 while (question := input('Question:\n')) == '':
                     pass
                 while (answer := input('Answer:\n')) == '':
                     pass
-                flashcards[question] = answer
+                session.add(Flashcard(question=question, answer=answer))
+                session.commit()
             else:
                 print(f'{inp2} is not an option')
     elif inp1 == '2':
-        # iterate over all flashcards
-        if flashcards:
-            for key, value in flashcards.items():
-                while (ans := input(f'Question: {key}\n'
-                                    f'Please press "y" to see the answer '
+        # iterate over all flashcards from db
+        if flashcards := session.query(Flashcard).all():
+            for card in flashcards:
+                print(f'Question: {card.question}')
+                while (ans := input(f'Please press "y" to see the answer '
                                     f'or press "n" to skip:\n')) not in ['y', 'n']:
                     pass
                 if ans == 'y':
-                    print(f'Answer: {value}')
+                    print(f'Answer: {card.answer}')
         else:
             print('There is no flashcard to practice!')
     else:
